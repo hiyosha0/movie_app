0bccb8e43a413213d1ac51874debc9f52c4b9a48
Hiyosha Vinupama <xvhiyosha12@gmail.com>
2025-11-20 20:53:22 +0530

    Add initial setup for NativeWind and create basic app structure

---
+++ components/SearchBar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+import { icons } from "../constants/icons"; // Ensure the path to icons is correct
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+  value: string;
+  onChangeText: (text: string) => void;
+}
+
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
+  return (
+    <View className=" flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className=" size-6"
+        resizeMode="contain"
+        style={{ tintColor: "#ab8bff" }}
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor={"#a8b5db"}
+        className=" flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
