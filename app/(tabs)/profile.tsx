22fbd5884e4e252345912d05ce17a1d499cfc71c
Hiyosha Vinupama <xvhiyosha12@gmail.com>
2025-11-21 16:04:48 +0530

    Enhance profile and saved tabs with icons and improved layout; implement movie details fetching and display

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className=" bg-primary flex-1">
+      <View className=" flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className=" size-10" tintColor={"#Fff"} />
+        <Text className=" text-white">Profile</Text>
+      </View>
     </View>
   );
 };
--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,10 +1,93 @@
+import { icons } from "@/constants/icons";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+
+interface MovieInfoProps {
+  label: string;
+  value?: string | null | number;
+}
+
+const MovieInfo = ({ label, value }: MovieInfoProps) => (
+  <View className=" flex-col items-start justify-center mt-5">
+    <Text className=" text-gray-300 font-normal text-sm">{label}</Text>
+    <Text className=" text-gray-200 font-bold text-sm mt-2">
+      {value || "n/a"}
+    </Text>
+  </View>
+);
 
 const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
   return (
-    <View>
-      <Text>MovieDetails</Text>
+    <View className=" bg-primary flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className=" w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+        <View className=" flex-col items-start justify-center mt-5 px-5">
+          <Text className=" text-white font-bold text-xl">{movie?.title}</Text>
+          <View className=" flex-row items-center gap-x-1 mt-2">
+            <Text className=" text-gray-200 text-sm">
+              {movie?.release_date?.split("-")[0]} |
+            </Text>
+            <Text className=" text-gray-200 text-sm">{movie?.runtime}m</Text>
+          </View>
+          <View className=" flex-row items-center bg-gray-800 px-2 py-1 rounded-md gap-x-1 mt-2">
+            <Image source={icons.star} className=" size-4" />
+            <Text className=" text-white font-bold text-sm">
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+            <Text className=" text-gray-300 text-sm">
+              ({movie?.vote_count} votes)
+            </Text>
+          </View>
+          <MovieInfo label="Overview" value={movie?.overview} />
+          <MovieInfo
+            label="Genres"
+            value={movie?.genres?.map((g) => g.name).join(" - ") || "n/a"}
+          />
+          <View className=" flex flex-row justify-between w-1/2 ">
+            <MovieInfo
+              label="Budget"
+              value={`$${movie?.budget / 1_000_000} million`}
+            />
+            <MovieInfo
+              label="Revenue"
+              value={`$${Math.round(movie?.revenue) / 1_000_000} `}
+            />
+          </View>
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies.map((c) => c.name).join("-") || "n/a"
+            }
+          />
+        </View>
+      </ScrollView>
+      <TouchableOpacity
+        className=" absolute bottom-5 left-0 right-0 mx-5 bg-accentText rounded-lg py-3.5 flex-row items-center 
+      justify-center z-10"
+        onPress={router.back}
+      >
+        <Image
+          source={icons.arrow}
+          className=" size-5 mr-1 mt-0.5 rotate-180"
+          tintColor={"#fff"}
+        />
+        <Text className=" text-white font-semibold text-base">Go back</Text>
+      </TouchableOpacity>
     </View>
   );
 };
